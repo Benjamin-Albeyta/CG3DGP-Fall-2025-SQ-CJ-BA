145c591d6f027c59918b50f6e7b868b903bb3c6d
Benjamin-Albeyta <balbeyta@gmail.com>
2025-10-14 23:13:54 -0400

    Reformatted Textures so properly implemented, added a view change potion

---
+++ Shepard of Dreams/Assets/Scripts/TextureChangeItem.cs
@@ -0,0 +1,29 @@
+/**
+  * Author: Benjamin Albeyta
+  * Date Created: 10/14/2025
+  * Date Last Updated: 10/14/2025
+  * Summary: Creates an item which when collected triggers a change in some textures in the level (tempoary but an implementation of a view change effect)
+  */
+
+using UnityEngine;
+
+public class TextureChangeItem : MonoBehaviour
+{
+    [Tooltip("The object whose texture will change when collected.")]
+    public TextureChanger target;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Only trigger when the player touches it
+        if (other.CompareTag("Player"))
+        {
+            if (target != null)
+            {
+                target.ChangeTexture();
+            }
+
+            // Destroy the item after collection
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Shepard of Dreams/Assets/Scripts/TextureChanger.cs
@@ -0,0 +1,45 @@
+/**
+  * Author: Benjamin Albeyta
+  * Date Created: 10/14/2025
+  * Date Last Updated: 10/14/2025
+  * Summary: Changes textures of a given object in the level
+  */
+
+using UnityEngine;
+
+public class TextureChanger : MonoBehaviour
+{
+    [Tooltip("The renderer whose texture will change.")]
+    public Renderer targetRenderer;
+
+    [Tooltip("The new material or texture to apply when triggered.")]
+    public Material newMaterial;
+
+    private Material originalMaterial;
+
+    private void Awake()
+    {
+
+        //When awake get the current component
+        if (targetRenderer == null)
+            targetRenderer = GetComponent<Renderer>();
+
+        originalMaterial = targetRenderer.material;
+    }
+
+    //When executed changes texture
+    public void ChangeTexture()
+    {
+        if (newMaterial != null)
+        {
+            targetRenderer.material = newMaterial;
+        }
+    }
+
+
+    //When executed resets textures back to original state 
+    public void ResetTexture()
+    {
+        targetRenderer.material = originalMaterial;
+    }
+}
