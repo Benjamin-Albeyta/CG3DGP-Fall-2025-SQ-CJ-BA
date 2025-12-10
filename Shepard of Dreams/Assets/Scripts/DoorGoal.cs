213935813759089aa7ecae810ebea152e06fc341
Benjamin-Albeyta <balbeyta@gmail.com>
2025-12-10 16:13:34 -0500

    Made the door only activate once

--- Shepard of Dreams/Assets/Scripts/DoorGoal.cs
+++ Shepard of Dreams/Assets/Scripts/DoorGoal.cs
@@ -2,8 +2,10 @@
   * Author: Sophia Qian, Benjamin Albeyta
   * Project Members: Caroline Jia, Benjamin Albeyta, Sophia Qian
   * Date Created: 9/20/2025
-  * Date Last Updated: 10/12/2025
+  * Date Last Updated: 12/10/2025
   * Summary: Responsible for keeping track of the end level objective, when player touches it, causes them to end the level and move on to the next one, through calling GameManager
+
+  * Update: Made it so that it can only be activated once
   */
 
 using System.Collections;
@@ -38,6 +40,9 @@ public class DoorGoal : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
+        var col = GetComponent<Collider>();
+        if (col != null)
+            col.enabled = false;
         if (!other.CompareTag("Player")) return;
         Debug.Log("[Goal] Door touched -> Win");
         clockScript.isRotating = true;
